6ef8e5a86cc4dee30354aa2c878e9dfe1e63d4ab
Daniel Weill <daniel.zalmen@gmail.com>
2023-12-03 14:19:48 -0600

    add route for profile

---
+++ villainary-react/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material';
+import Profile from './Profile';
+
+export default function Page() {
+  return (
+    <Container maxWidth="sm">
+      <Profile />
+    </Container>
+  );
+}
