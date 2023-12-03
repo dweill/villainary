ff81a4c0cba77ddaf70296910dc5b6b8a2b5559c
Daniel Weill <daniel.zalmen@gmail.com>
2023-12-03 14:19:05 -0600

    update villain intake form

--- villainary-react/app/VillainIntakeForm.tsx
+++ villainary-react/app/VillainIntakeForm.tsx
@@ -2,11 +2,9 @@ import { Button, Grid, TextField } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import { theme } from './Theme';
+import { IntakeFormInputs } from './interfaces/IntakeFormInputs';
 import { setVillainName } from './state/userState.slice';
-
-type Inputs = {
-  villainName: string;
-};
 
 export default function VillainIntakeForm() {
   const {
@@ -14,29 +12,37 @@ export default function VillainIntakeForm() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<IntakeFormInputs>();
 
   const router = useRouter();
   const dispatch = useDispatch();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
+  const onSubmit: SubmitHandler<IntakeFormInputs> = (data) => {
     dispatch(setVillainName(data.villainName));
-    router.push('/test');
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid container rowSpacing={1} columnSpacing={3}>
-        <Grid item xs={12}>
+      <Grid container rowSpacing={1} columnSpacing={3} alignItems="center">
+        <Grid item xs={8}>
           <TextField
             fullWidth
             placeholder="Seize your identity"
             {...register('villainName', { required: true })}
+            sx={{ backgroundColor: theme.palette.primary.main }}
           />
           {errors.villainName && <span>This field is required</span>}
         </Grid>
-        <Grid item xs={8} />
         <Grid item xs={4}>
-          <Button className="block mt-12" variant="contained" type="submit">
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.main,
+              '&:hover': { backgroundColor: theme.palette.secondary.light },
+            }}
+            className="block mt-12"
+            variant="contained"
+            type="submit"
+            color="secondary"
+          >
             Become a Villain
           </Button>
         </Grid>
