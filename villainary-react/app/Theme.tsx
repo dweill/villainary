70beffaf41e10a21c63921fe194532a44cd3634c
Daniel Weill <daniel.zalmen@gmail.com>
2023-12-26 22:28:11 -0600

    modify theme colors slightly

--- villainary-react/app/Theme.tsx
+++ villainary-react/app/Theme.tsx
@@ -1,24 +1,24 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
-  palette: {
-    background: {
-      default: '#000',
+    palette: {
+        background: {
+            default: '#262626',
+        },
+        text: {
+            primary: '#fff',
+        },
+        primary: {
+            light: '#555555',
+            main: '#262626',
+            dark: '#000000',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff0000',
+            main: '#b10000',
+            dark: '#990000',
+            contrastText: '#fff',
+        },
     },
-    text: {
-      primary: '#fff',
-    },
-    primary: {
-      light: '#555555',
-      main: '#262626',
-      dark: '#000000',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff200c',
-      main: '#ff0000',
-      dark: '#da0000',
-      contrastText: '#000',
-    },
-  },
 });
