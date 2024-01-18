a181f1961fa82d5cd4830c39646df6ab27c6f0c0
Daniel Weill <daniel.zalmen@gmail.com>
2024-01-17 20:03:00 -0600

    add wallet to product page

--- villainary-react/app/products/page.tsx
+++ villainary-react/app/products/page.tsx
@@ -1,15 +1,21 @@
 import { Grid } from '@mui/material';
+import Wallet from '../components/Wallet';
 import { products } from '../constants/products';
 import ProductCard from './ProductCard';
 
 export default function page() {
     return (
-        <Grid container rowSpacing={3} columnSpacing={3}>
-            {products.map((product) => (
-                <Grid item xs={6}>
-                    <ProductCard product={product} key={product.id} />
-                </Grid>
-            ))}
+        <Grid container rowSpacing={3}>
+            <Grid item>
+                <Wallet  />
+            </Grid>
+            <Grid item container rowSpacing={3} columnSpacing={3}>
+                {products.map((product) => (
+                    <Grid item xs={6} key={product.id}>
+                        <ProductCard product={product} />
+                    </Grid>
+                ))}
+            </Grid>
         </Grid>
     );
 }
