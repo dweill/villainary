5e2e8d2188e9bd2201bbe34f6cc2b635612ea1cf
Daniel Weill <daniel.zalmen@gmail.com>
2024-01-29 22:18:55 -0600

    add wavy route loader

--- villainary-react/app/layout.tsx
+++ villainary-react/app/layout.tsx
@@ -5,25 +5,25 @@ import './globals.css';
 import Providers from './providers/Provider';
 
 export const metadata: Metadata = {
-  title: 'Villainary',
-  description: 'Become the Villain in your own story',
+    title: 'Villainary',
+    description: 'Become the Villain in your own story',
 };
 
 // app/layout.js
 export default function RootLayout(props: { children: any }) {
-  const { children } = props;
-  return (
-    <html lang="en">
-      <body className="body">
-        <Providers>
-          <ThemeRegistry options={{ key: 'mui' }}>
-            <Navbar></Navbar>
-            <main className="flex min-h-screen  items-center justify-center p-24">
-              {children}
-            </main>
-          </ThemeRegistry>
-        </Providers>
-      </body>
-    </html>
-  );
+    const { children } = props;
+    return (
+        <html lang="en">
+            <body className="body">
+                <Providers>
+                    <ThemeRegistry options={{ key: 'mui' }}>
+                        <Navbar></Navbar>
+                        <main className="flex min-h-screen  items-center justify-center p-24">
+                            {children}
+                        </main>
+                    </ThemeRegistry>
+                </Providers>
+            </body>
+        </html>
+    );
 }
